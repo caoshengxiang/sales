fc884d964e62fcf89773bff3994f98ccf6eb0441
caoshengxiang <1104729815@qq.com>
2018-04-27 17:46:13 +0800

    修改目录名

--- src/router/index.js
+++ src/router/index.js
@@ -3,17 +3,17 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
 // 登陆，重置
-const signIn = resolve => require.ensure([], () => resolve(require('../pages/sign/signIn')), 'sign')
-const resetPassword = resolve => require.ensure([], () => resolve(require('../pages/sign/resetPassword')), 'sign')
+const signIn = resolve => require.ensure([], () => resolve(require('../view/sign/signIn')), 'sign')
+const resetPassword = resolve => require.ensure([], () => resolve(require('../view/sign/resetPassword')), 'sign')
 
 // home
-const indexPage = resolve => require.ensure([], () => resolve(require('../pages/index.vue')), 'index')
+const indexPage = resolve => require.ensure([], () => resolve(require('../view/index.vue')), 'index')
 
 // 销售
-const saleHome = resolve => require.ensure([], () => resolve(require('../pages/saleMange/saleHome/saleHome')), 'saleHome')
+const saleHome = resolve => require.ensure([], () => resolve(require('../view/saleManage/saleHome/saleHome')), 'saleHome')
 
 // 企业管理
-const manageHome = resolve => require.ensure([], () => resolve(require('../pages/companyManage/manageHome/manageHome')), 'manageHome')
+const manageHome = resolve => require.ensure([], () => resolve(require('../view/companyManage/manageHome/manageHome')), 'manageHome')
 
 Vue.use(Router)
 
