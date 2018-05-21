b010900db3dcdb5538c4677c816e44e3717c0202
caoshengxiang <1104729815@qq.com>
2018-05-21 16:12:36 +0800

    添加接口返回的拦截器统一处理，错误日志

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,6 +1,20 @@
 import { serverUrl } from './const'
+import { Message } from 'element-ui'
 
 $axios.defaults.baseURL = serverUrl
-$axios.defaults.timeout = 300000
+$axios.defaults.timeout = 100000
 // $axios.defaults.headers = {'X-Custom-Header': 'foobar'}
 // $axios.defaults.headers.post['content-Type'] = 'appliction/x-www-form-urlencoded'
+
+//添加一个返回拦截器
+$axios.interceptors.response.use((response) => {
+  //对返回的数据进行一些处理
+  // console.log(response)
+  if (!response.data.status) { // 后台返回错误
+    Message.error(response.data.error.message)
+  }
+  return response
+}, (error) => {
+  //对返回的错误进行一些处理
+  return Promise.reject(error)
+})
