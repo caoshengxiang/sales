d2729b6c8a3b1ac384a5bcc3fbfab801d04e9b89
yangzailin <908869887@qq.com>
2018-05-27 18:30:41 +0800

    记住密码

--- src/view/sign/signIn.vue
+++ src/view/sign/signIn.vue
@@ -63,10 +63,51 @@
     components: {
       feElement
     },
+    mounted() {
+      this.getCookie();
+    },
     methods: {
+      //设置cookie
+      setCookie(c_name, c_pwd, c_isRemember,exdays) {
+        var exdate = new Date(); //获取时间
+        exdate.setTime(exdate.getTime() + 24 * 60 * 60 * 1000 * exdays); //保存的天数
+        //字符串拼接cookie
+        window.document.cookie = "userName" + "=" + c_name + ";path=/;expires=" + exdate.toGMTString();
+        window.document.cookie = "userPwd" + "=" + c_pwd + ";path=/;expires=" + exdate.toGMTString();
+        window.document.cookie = "checked" + "=" + c_isRemember + ";path=/;expires=" + exdate.toGMTString();
+      },
+      //读取cookie
+      getCookie: function() {
+        if (document.cookie.length > 0) {
+          var arr = document.cookie.split('; '); //这里显示的格式需要切割一下自己可输出看下
+          for (var i = 0; i < arr.length; i++) {
+            var arr2 = arr[i].split('='); //再次切割
+            //判断查找相对应的值
+            if (arr2[0] == 'userName') {
+              this.formData.username = arr2[1]; //保存到保存数据的地方
+            } else if (arr2[0] == 'userPwd') {
+              this.formData.password = arr2[1];
+            }
+            else if(arr2[0] == 'checked') {
+              this.isRemember = arr2[1]
+            }
+          }
+        }
+      },
+      //清除cookie
+      clearCookie: function() {
+        this.setCookie("", "", -1); //修改2值都为空，天数为负1天就好了
+      },
       submitForm (formName) {
         this.$refs[formName].validate((valid) => {
           if (valid) {
+            if(this.isRemember){
+              this.setCookie(this.formData.username, this.formData.password,this.isRemember, 7)
+            }
+            else{
+              this.clearCookie()
+            }
+
             var pwd  = sha1(this.formData.password);
             API.login.login({account:this.formData.username,pwd:pwd,client:2}, (res) => {
               this.loading = false;
@@ -76,7 +117,9 @@
               }
               else
               {
-                this.$router.push({name: 'resetPassword', params: {end: 'ME'}})
+                if(res.error.statusCode === "10011") {
+                  this.$router.push({name: 'resetPassword', params: {end: 'ME'}})
+                }
               }
             }, (mock) => {
               this.loading = false;
@@ -85,12 +128,6 @@
                 type: 'error'
               });
             })
-            let ss = {
-              name:"用户姓名",
-              authKey:"eydhbGcnOidIUzI1NicsJ3R5cGUnOidKV1QnfQ%3D%3D.eyJ1c2VyIjoiMDQ2MkYwOEVBQzEwMDE2RTNCMEE1NkJERDExMjdGM0YiLCJ2YWxpZFBlcmlvZCI6MTAzNjgwMDAwMDAsImlhdCI6MTUyNzM0OTg0NDA3NH0%3D.%2BVaOKCw8Abt3mo7%2BB6dDvZOfOcDoPo6Ft4fuDh%2BBvaU%3D"
-            }
-            webStorage.setItem('userInfo', ss);
-            this.$router.push({name: 'saleHome', params: {end: 'ME'}})
           } else {
             console.log('error submit!!')
             return false
