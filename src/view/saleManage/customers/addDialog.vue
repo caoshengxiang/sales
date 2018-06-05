daa6163de2e5161d75f2e6bcf215afd8c5ed769f
caoshengxiang <1104729815@qq.com>
2018-06-05 18:02:22 +0800

    地区

--- src/view/saleManage/customers/addDialog.vue
+++ src/view/saleManage/customers/addDialog.vue
@@ -61,7 +61,7 @@
                   }">
                 </el-cascader>-->
                 <AreaSelect ref="areaSe"
-                            :area="addForm.provinceName + ' ' + addForm.cityName + ' ' + addForm.areaName"
+                            :area="(addForm.provinceName?addForm.provinceName:'') + ' ' + (addForm.cityName?addForm.cityName:'')  + ' ' + (addForm.areaName?addForm.areaName:'')"
                             @change="areaSelectedOptionsHandleChange"
                             :selectLastLevelMode="true"></AreaSelect>
               </el-form-item>
