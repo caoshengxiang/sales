93d5553a06a49b1bdf0aae140bc1f9534b628e86
yangzailin <908869887@qq.com>
2018-07-19 17:57:11 +0800

    xiu

--- src/utils/api.js
+++ src/utils/api.js
@@ -1298,7 +1298,7 @@ export default {
     update (params, success, error) {
       $axios({
         method: 'put',
-        url: 'region',
+        url: 'region/' + params.id,
         data: params,
       }).then((res) => {
         success && success(res.data)
