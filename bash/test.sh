5f5a98bfd9c0b68ac0e61a4033964d6ab4a51632
caoshengxiang <1104729815@qq.com>
2018-08-02 18:25:08 +0800

    打包配置

--- bash/test.sh
+++ bash/test.sh
@@ -1,9 +1,9 @@
 #!  /bin/bash
 # && 如果前面的命令正确执行，那么后面的命令也将执行。否则后面的命令不执行。
-mv dist sales && tar zcvf sales.tar.gz sales && mv sales dist && mv sales.tar.gz dist/
-scp dist/sales.tar.gz root@119.27.160.41:/opt
+mv dist sales && tar zcvf sales.tar.gz sales && mv sales dist
+scp sales.tar.gz root@119.27.160.41:/opt
 ssh root@119.27.160.41 "rm -rf /opt/web-front/sales && tar -zxvf /opt/sales.tar.gz -C /opt/web-front"
-rm -rf dist
+# rm -rf dist
 
 # daydayUP@2017
 # http://sales.dcstar-inc.com/#/
