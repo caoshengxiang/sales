764c9c1d65288a454d3ff499691acb06a8e26b71
caoshengxiang <1104729815@qq.com>
2018-08-02 19:51:05 +0800

    打包配置文件

--- bash/compress.sh
+++ bash/compress.sh
@@ -1,3 +1,3 @@
 #!  /bin/bash
 # && 如果前面的命令正确执行，那么后面的命令也将执行。否则后面的命令不执行。
-mv dist temp && tar zcvf temp.tar.gz temp && mv temp dist && mv temp.tar.gz dist/
+mv dist sales && tar zcvf sales.tar.gz sales && mv sales dist
