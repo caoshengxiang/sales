af9d0327d8fba7251c5deef0c4dc3671a69091eb
caoshengxiang <1104729815@qq.com>
2018-08-14 19:45:37 +0800

    sh

--- bash/release.sh
+++ bash/release.sh
@@ -4,3 +4,4 @@ scp dist.tar.gz root@119.23.106.240:/opt
 ssh root@119.23.106.240 "rm -rf /opt/web-front/sales/bak && mkdir /opt/web-front/sales/bak && mv /opt/web-front/sales/index.html /opt/web-front/sales/static -t /opt/web-front/sales/bak/ && tar -zxvf /opt/dist.tar.gz -C /opt/web-front/sales/ && mv /opt/web-front/sales/dist/* -t /opt/web-front/sales/ && rm -rf /opt/web-front/sales/dist"
 
 # ***
+http://erp.zzcfo.cn
