aab430dc2d683fc0ea16b394042ba11dd5a22c80
caoshengxiang <1104729815@qq.com>
2018-11-02 10:40:08 +0800

    工单

--- src/view/saleManage/PMS/serviceOrderList/detail.vue
+++ src/view/saleManage/PMS/serviceOrderList/detail.vue
@@ -196,69 +196,7 @@
             </table>
           </el-tab-pane>
           <el-tab-pane label="订单加工信息" name="operate">
-            <table class="detail-table">
-              <tr>
-                <td style="height: 50px;" class="td-center">外勤服务[张三]</td>
-                <td colspan="5">
-                  <icon-text icon="el-icon-picture" text="首次电话沟通" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="首次上门沟通" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="录入客户资料" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="装订凭证" status="1"></icon-text>
-                </td>
-              </tr>
-              <tr>
-                <td style="height: 50px;" class="td-center">财务记账[张三]</td>
-                <td colspan="5">
-                  <icon-text icon="el-icon-picture" text="客户资料审核" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="客户票据审核" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="记账日常告知" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="出具财务报表" status="1"></icon-text>
-                </td>
-              </tr>
-              <tr>
-                <td style="height: 50px;" class="td-center">财务申报[张三]</td>
-                <td colspan="5">
-                  <icon-text icon="el-icon-picture" text="税前沟通" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="纳税申报" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="纳税异常提醒" status="1"></icon-text>
-                </td>
-              </tr>
-              <tr>
-                <td style="height: 50px;" class="td-center">财务内控[张三]</td>
-                <td colspan="5">
-                  <icon-text icon="el-icon-picture" text="财务内控分析" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="财务内控构建指导" status="1"></icon-text>
-                </td>
-              </tr>
-              <tr>
-                <td style="height: 50px;" class="td-center">税收风控[张三]</td>
-                <td colspan="5">
-                  <icon-text icon="el-icon-picture" text="增值税发票风险管理指导" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="税收优惠政策辅导" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="税收问题提醒" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="税收分控方案设计" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="纳税信用等级维护指导" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="财税专家“一对一”咨询" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="税务危机应对参与" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="税收策划指导" status="1"></icon-text>
-                </td>
-              </tr>
-              <tr>
-                <td style="height: 50px;" class="td-center">金融服务[张三]</td>
-                <td colspan="5">
-                  <icon-text icon="el-icon-picture" text="融资需求分析" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="融资对接指导" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="政府扶持资金规话" status="1"></icon-text>
-                </td>
-              </tr>
-              <tr>
-                <td style="height: 50px;" class="td-center">其他单项服务（产品名）[张三]</td>
-                <td colspan="5">
-                  <icon-text icon="el-icon-picture" text="开始服务" status="1"></icon-text>
-                  <icon-text icon="el-icon-picture" text="完成服务" status="1"></icon-text>
-                </td>
-              </tr>
-            </table>
+            <working-op :order-id="$route.query.orderId" :workOrderId="$route.query.id" :customerName="detail.serviceName"></working-op>
           </el-tab-pane>
         </el-tabs>
       </div>
@@ -271,6 +209,7 @@
   import API from '../../../../utils/api'
   import { mapState } from 'vuex'
   import orderDialog from './orderDialog'
+  import workingOp from '../serviceWorkOrderList/workingOp'
 
   export default {
     name: 'detail',
@@ -300,6 +239,7 @@
     },
     components: {
       iconText,
+      workingOp,
     },
     methods: {
       operateOptions () {
