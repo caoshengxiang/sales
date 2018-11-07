339a37f85b5f7bca0d2b9d9944eda0393becc36c
caoshengxiang <1104729815@qq.com>
2018-11-07 18:41:16 +0800

    添加订单，地区选项永远置灰，todo 考虑后面有没有逻辑问题

--- src/view/saleManage/CRM/salesOrders/addDialog.vue
+++ src/view/saleManage/CRM/salesOrders/addDialog.vue
@@ -85,8 +85,9 @@
             <td class="td-title">地区</td>
             <td class="td-text">
               <el-form-item prop="provinceId">
+                <!--:disabled="(orderState===0 || addForm.chanceId || params.fromChance)?true:false"-->
               <AreaSelect ref="areaSe"
-                          :disabled="(orderState===0 || addForm.chanceId || params.fromChance)?true:false"
+                          :disabled="true"
                           :area="(addForm.provinceName?addForm.provinceName:'') + ' ' + (addForm.cityName?addForm.cityName:'')  + ' ' + (addForm.areaName?addForm.areaName:'')"
                           @change="areaSelectedOptionsHandleChange"
                           :selectLastLevelMode="true"></AreaSelect>
