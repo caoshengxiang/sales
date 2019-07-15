e4726ff83a601909ca4c93b6e523a3303c538971
何天龙 <rstrtt@163.com>
2019-07-15 14:14:35 +0800

    订单原评价星级如果是4星或5星，直接同步订单回访星级的数据也是对应星级

--- src/view/saleManage/CSM/serviceReturnVisit/detail.vue
+++ src/view/saleManage/CSM/serviceReturnVisit/detail.vue
@@ -204,8 +204,12 @@
                   </el-table-column>
                   <el-table-column label="回复星级" prop="retEvaluation">
                     <template slot-scope="scope">
-                      <span v-if="scope.row.retEvaluation">{{scope.row.retEvaluation}}星</span>
-                      <span v-if="!scope.row.retEvaluation"></span></template>
+                      <span v-if="scope.row.reviewScore > 3">{{scope.row.reviewScore}}星</span>
+                      <span v-if="scope.row.reviewScore <= 3">
+                        <span v-if="scope.row.retEvaluation">{{scope.row.retEvaluation}}星</span>
+                        <span v-if="!scope.row.retEvaluation"></span>
+                      </span>
+                    </template>
                   </el-table-column>
                   <el-table-column label="回复状态" prop="state">
                     <template slot-scope="scope">
